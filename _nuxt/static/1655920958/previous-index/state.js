b3e6a3e77fcfea45956a55ae898d490d433759e0
葉奕緯 <frank@yeyiweideMacBook-Air.local>
2022-06-23 09:27:09 +0800

    change icon

--- _nuxt/static/1655920958/previous-index/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsugardaddy\u002F_nuxt\u002Fstatic\u002F1655920958",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fprevious-index",config:{_app:{basePath:"\u002Fsugardaddy\u002F",assetsPath:"\u002Fsugardaddy\u002F_nuxt\u002F",cdnURL:a}}}}(null));
